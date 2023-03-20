6374ae7a34f34150b13d1c338bddd39107fd2b28
sejaly <sejalyadav0602@gmail.com>
2023-03-20 14:48:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+mycon_index = document.getElementById('mycon-index')
+mycon_news1 = document.getElementById('mycon-news1')
+mycon_news2 = document.getElementById('mycon-news2')
+mycon_news3 = document.getElementById('mycon-news3')
+mycon_news4 = document.getElementById('mycon-news4')
+mycon_news5 = document.getElementById('mycon-news5')
+function index() {
+  mycon_index.style.display = "flex"
+  mycon_news1.style.display = "none"
+  mycon_news2.style.display = "none"
+  mycon_news3.style.display = "none"
+  mycon_news4.style.display = "none"
+  mycon_news5.style.display = "none"
+}
+function news1() {
+  mycon_index.style.display = "none"
+  mycon_news1.style.display = "flex"
+  mycon_news2.style.display = "none"
+  mycon_news3.style.display = "none"
+  mycon_news4.style.display = "none"
+  mycon_news5.style.display = "none"
+}
+function news2() {
+  mycon_index.style.display = "none"
+  mycon_news1.style.display = "none"
+  mycon_news2.style.display = "flex"
+  mycon_news3.style.display = "none"
+  mycon_news4.style.display = "none"
+  mycon_news5.style.display = "none"
+}
+function news3() {
+  mycon_index.style.display = "none"
+  mycon_news1.style.display = "none"
+  mycon_news2.style.display = "none"
+  mycon_news3.style.display = "flex"
+  mycon_news4.style.display = "none"
+  mycon_news5.style.display = "none"
+}
+function news4() {
+  mycon_index.style.display = "none"
+  mycon_news1.style.display = "none"
+  mycon_news2.style.display = "none"
+  mycon_news3.style.display = "none"
+  mycon_news4.style.display = "flex"
+  mycon_news5.style.display = "none"
+}
+function news5() {
+  mycon_index.style.display = "none"
+  mycon_news1.style.display = "none"
+  mycon_news2.style.display = "none"
+  mycon_news3.style.display = "none"
+  mycon_news4.style.display = "none"
+  mycon_news5.style.display = "flex"
+}
